f8fdd511b6dca72be3d28f19446b2a3d75694fb6
Aleksandar Milinkovic <milinkovicaleks@gmail.com>
2021-05-09 18:25:14 +0200

    update v2

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          I am an Example App Running in the Cloud
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
